0c01875f6af6d36c2954c09c9464ea45ecd8f648
nobody <nobody@28c7f869-5b4e-e670-f602-82bfaf57f300>
2005-01-08 01:09:56 +0000

    This commit was manufactured by cvs2svn to create tag 'postmerge-1_4-1_3_2-beta2'.

---
+++ tags/postmerge-1_4-1_3_2-beta2/src/VirtualInputDevice/HelloUniverse.java
@@ -0,0 +1,134 @@
+/*
+ * $RCSfile$
+ *
+ * Copyright (c) 2004 Sun Microsystems, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistribution of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistribution in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * Neither the name of Sun Microsystems, Inc. or the names of
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * This software is provided "AS IS," without a warranty of any
+ * kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND
+ * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY
+ * EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL
+ * NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF
+ * USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR
+ * ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL,
+ * CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND
+ * REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR
+ * INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES.
+ *
+ * You acknowledge that this software is not designed, licensed or
+ * intended for use in the design, construction, operation or
+ * maintenance of any nuclear facility.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+import com.sun.j3d.utils.applet.MainFrame;
+import com.sun.j3d.utils.geometry.ColorCube;
+import com.sun.j3d.utils.universe.*;
+import javax.media.j3d.*;
+import javax.vecmath.*;
+
+public class HelloUniverse extends Applet {
+
+    private SimpleUniverse u = null;
+
+    public BranchGroup createSceneGraph() {
+
+	BranchGroup objRoot = new BranchGroup();
+	TransformGroup objTrans = new TransformGroup();
+	objTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+	objRoot.addChild(objTrans);
+	objTrans.addChild(new ColorCube(0.2));
+	Transform3D yAxis = new Transform3D();
+	Alpha rotationAlpha = new Alpha(-1, Alpha.INCREASING_ENABLE,
+					0, 0,
+					4000, 0, 0,
+					0, 0, 0);
+	RotationInterpolator rotator =
+	    new RotationInterpolator(rotationAlpha, objTrans, yAxis,
+				     0.0f, (float) Math.PI*2.0f);
+	BoundingSphere bounds =
+	    new BoundingSphere(new Point3d(0.0,0.0,0.0), 100.0);
+	rotator.setSchedulingBounds(bounds);
+	objTrans.addChild(rotator);
+	return objRoot;
+    }
+
+
+    public HelloUniverse() {
+
+    }
+
+    public void init() {
+	// These are the string arguments given to the VirtualInputDevice
+        // constructor.  These are settable parameters.  Look in the 
+        // VirtualInputDevice constructor for a complete list.
+        String[] args = new String[10];
+        args[0] = "printvalues";
+        args[1] = "true";
+        args[2] = "yscreeninitloc";
+        args[3] = "50";
+        args[4] = null;
+
+        InputDevice device = new VirtualInputDevice( args );
+
+        // now create the HelloUniverse Canvas
+	setLayout(new BorderLayout());
+        GraphicsConfiguration config =
+           SimpleUniverse.getPreferredConfiguration();
+
+        Canvas3D c = new Canvas3D(config);
+	add("Center", c);
+
+	// Create a simple scene and attach it to the virtual universe
+	BranchGroup scene = createSceneGraph();
+	u = new SimpleUniverse(c);
+
+        // The InputDevice must be initialized before registering it 
+        // with the PhysicalEnvironment object.
+        device.initialize();
+
+	// Register the VirtualInputDevice with Java 3D
+	u.getViewer().getPhysicalEnvironment().addInputDevice( device );
+
+	TransformGroup viewTrans = 
+                        u.getViewingPlatform().getViewPlatformTransform();
+	SensorBehavior s = new SensorBehavior( viewTrans, device.getSensor(0) );
+	s.setSchedulingBounds( new BoundingSphere
+                       ( new Point3d(0.0,0.0,0.0), Float.MAX_VALUE ));
+	scene.addChild( s );
+	u.addBranchGraph(scene);
+    }
+
+    public void destroy() {
+	u.cleanup();
+    }
+
+
+    public static void main(String[] args) {
+	new MainFrame(new HelloUniverse(), 350, 350);
+    }
+}
