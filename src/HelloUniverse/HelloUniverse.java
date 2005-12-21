65b559a9e592a2c013eab3ee18283a403da280b0
kcr <kcr@28c7f869-5b4e-e670-f602-82bfaf57f300>
2005-12-21 01:12:49 +0000

    More changes for Issue 206 : Example program cleanup

--- src/HelloUniverse/HelloUniverse.java
+++ src/HelloUniverse/HelloUniverse.java
@@ -106,6 +106,9 @@ public class HelloUniverse extends javax.swing.JFrame {
 	// objects in the scene can be viewed.
 	univ.getViewingPlatform().setNominalViewingTransform();
 
+	// Ensure at least 5 msec per frame (i.e., < 200Hz)
+	univ.getViewer().getView().setMinimumFrameCycleTime(5);
+
 	return c;
     }
 
