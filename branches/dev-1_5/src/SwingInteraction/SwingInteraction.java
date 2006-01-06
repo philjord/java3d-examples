9b650c1e365f617dc54e163b98fb1ef871b5f01d
nobody <nobody@28c7f869-5b4e-e670-f602-82bfaf57f300>
2006-01-06 23:33:53 +0000

    This commit was manufactured by cvs2svn to create branch 'dev-1_5'.

---
+++ branches/dev-1_5/src/SwingInteraction/SwingInteraction.java
@@ -0,0 +1,250 @@
+/*
+ * $RCSfile$
+ *
+ * Copyright (c) 2006 Sun Microsystems, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistribution of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistribution in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * Neither the name of Sun Microsystems, Inc. or the names of
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * This software is provided "AS IS," without a warranty of any
+ * kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND
+ * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY
+ * EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL
+ * NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF
+ * USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR
+ * ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL,
+ * CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND
+ * REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR
+ * INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES.
+ *
+ * You acknowledge that this software is not designed, licensed or
+ * intended for use in the design, construction, operation or
+ * maintenance of any nuclear facility.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+
+import com.sun.j3d.utils.universe.*;
+import com.sun.j3d.utils.geometry.ColorCube;
+import javax.media.j3d.*;
+import javax.vecmath.*;
+import java.awt.GraphicsConfiguration;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.util.Enumeration;
+import javax.swing.JPopupMenu;
+
+/**
+ * Simple Java 3D test program created in NetBeans to illustrate interacting
+ * with a Java 3D scene graph from an Swing-based program.
+ */
+public class SwingInteraction extends javax.swing.JFrame {
+
+    private SimpleUniverse univ = null;
+    private BranchGroup scene = null;
+
+    private TransformGroup objTrans;
+    private RotateBehavior awtBehavior;
+
+    public BranchGroup createSceneGraph() {
+	// Create the root of the branch graph
+	BranchGroup objRoot = new BranchGroup();
+
+	// Create the transform group node and initialize it to the
+	// identity.  Enable the TRANSFORM_WRITE capability so that
+	// our behavior code can modify it at runtime.  Add it to the
+	// root of the subgraph.
+	objTrans = new TransformGroup();
+	objTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+	objRoot.addChild(objTrans);
+
+	// Create a simple shape leaf node, add it to the scene graph.
+	objTrans.addChild(new ColorCube(0.4));
+
+	// create the RotateBehavior	
+        awtBehavior = new RotateBehavior(objTrans);
+	BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0),
+						   100.0);
+	awtBehavior.setSchedulingBounds(bounds);
+	objRoot.addChild(awtBehavior);
+
+	return objRoot;
+    }
+
+    private Canvas3D createUniverse() {
+	GraphicsConfiguration config =
+	    SimpleUniverse.getPreferredConfiguration();
+
+	Canvas3D c = new Canvas3D(config);
+
+	univ = new SimpleUniverse(c);
+
+	// This will move the ViewPlatform back a bit so the
+	// objects in the scene can be viewed.
+        univ.getViewingPlatform().setNominalViewingTransform();
+
+	// Ensure at least 5 msec per frame (i.e., < 200Hz)
+	univ.getViewer().getView().setMinimumFrameCycleTime(5);
+
+	return c;
+    }
+
+    /**
+     * Creates new form SwingInteraction
+     */
+    public SwingInteraction() {
+        // Initialize the GUI components
+        JPopupMenu.setDefaultLightWeightPopupEnabled(false);
+        initComponents();
+
+        // Create Canvas3D and SimpleUniverse; add canvas to drawing panel
+        Canvas3D c = createUniverse();
+        drawingPanel.add(c, java.awt.BorderLayout.CENTER);
+
+        // Create the content branch and add it to the universe
+        scene = createSceneGraph();
+        univ.addBranchGraph(scene);
+    }
+
+    /**
+     * Behavior class that waits for a behavior post from the AWT event handler
+     */
+    class RotateBehavior extends Behavior {
+
+        private TransformGroup transformGroup;
+        private Transform3D trans = new Transform3D();
+        private WakeupCriterion criterion;
+        private float angle = 0.0f;
+
+        private final int ROTATE = 1;
+
+        // create a new RotateBehavior
+        RotateBehavior(TransformGroup tg) {
+            transformGroup = tg;
+        }
+
+        // initialize behavior to wakeup on a behavior post with id = ROTATE
+        public void initialize() {
+            criterion = new WakeupOnBehaviorPost(this, ROTATE);
+            wakeupOn(criterion);
+        }
+
+        // processStimulus to rotate the cube
+        public void processStimulus(Enumeration criteria) {
+            angle += Math.toRadians(10.0);
+            trans.rotY(angle);
+            transformGroup.setTransform(trans);
+            wakeupOn(criterion);
+        }
+
+        // when the mouse is clicked, postId for the behavior
+        void rotate() {
+            postId(ROTATE);
+        }
+    }
+
+    // ----------------------------------------------------------------
+    
+    /** This method is called from within the constructor to
+     * initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is
+     * always regenerated by the Form Editor.
+     */
+    // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        guiPanel = new javax.swing.JPanel();
+        rotateButton = new javax.swing.JButton();
+        drawingPanel = new javax.swing.JPanel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        fileMenu = new javax.swing.JMenu();
+        exitMenuItem = new javax.swing.JMenuItem();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Swing Interaction Test");
+        guiPanel.setLayout(new java.awt.GridBagLayout());
+
+        rotateButton.setText("Rotate");
+        rotateButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rotateButtonActionPerformed(evt);
+            }
+        });
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.insets = new java.awt.Insets(4, 4, 4, 4);
+        guiPanel.add(rotateButton, gridBagConstraints);
+
+        getContentPane().add(guiPanel, java.awt.BorderLayout.NORTH);
+
+        drawingPanel.setLayout(new java.awt.BorderLayout());
+
+        drawingPanel.setPreferredSize(new java.awt.Dimension(500, 500));
+        getContentPane().add(drawingPanel, java.awt.BorderLayout.CENTER);
+
+        fileMenu.setText("File");
+        exitMenuItem.setText("Exit");
+        exitMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                exitMenuItemActionPerformed(evt);
+            }
+        });
+
+        fileMenu.add(exitMenuItem);
+
+        jMenuBar1.add(fileMenu);
+
+        setJMenuBar(jMenuBar1);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void rotateButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rotateButtonActionPerformed
+        awtBehavior.rotate();
+    }//GEN-LAST:event_rotateButtonActionPerformed
+
+    private void exitMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitMenuItemActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_exitMenuItemActionPerformed
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new SwingInteraction().setVisible(true);
+            }
+        });
+    }
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel drawingPanel;
+    private javax.swing.JMenuItem exitMenuItem;
+    private javax.swing.JMenu fileMenu;
+    private javax.swing.JPanel guiPanel;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JButton rotateButton;
+    // End of variables declaration//GEN-END:variables
+    
+}
