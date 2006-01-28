3d596b24b72677fbf264a22f3c0ff44aa9bb2baf
jada <jada@28c7f869-5b4e-e670-f602-82bfaf57f300>
2006-01-28 16:24:24 +0000

    Fixed issue 190 : Need to modify all of Java 3D off-screen example programs to request a single-buffered graphics Configuration

--- src/OffScreenCanvas3D/PrintFromButton.java
+++ src/OffScreenCanvas3D/PrintFromButton.java
@@ -42,130 +42,219 @@
  * $State$
  */
 
-import com.sun.j3d.utils.geometry.ColorCube;
-import java.applet.Applet;
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import com.sun.j3d.utils.applet.MainFrame;
 import com.sun.j3d.utils.universe.*;
+import com.sun.j3d.utils.geometry.ColorCube;
 import javax.media.j3d.*;
 import javax.vecmath.*;
+import java.awt.GraphicsConfiguration;
+import javax.swing.JPopupMenu;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
- * PrintFromButton tests renderOffScreenBuffer from a button
+ * PrintFromButton programs with simple UI.
  */
-public class PrintFromButton extends Applet implements ActionListener {
+public class PrintFromButton extends javax.swing.JFrame {
 
-  OffScreenCanvas3D c;
+    private SimpleUniverse univ = null;
+    private BranchGroup scene = null;
+    private Raster drawRaster = null;    
+    private OffScreenCanvas3D offScreenCanvas = null;
 
-    private SimpleUniverse u = null;
+    public BranchGroup createSceneGraph() {
+	// Create the root of the branch graph
+	BranchGroup objRoot = new BranchGroup();
 
-  public BranchGroup createSceneGraph(Raster drawRaster)
-  {
-    // Create the root of the branch graph
-    BranchGroup objRoot = new BranchGroup();
+	// trans object has composited transformation matrix
+	Transform3D trans = new Transform3D();
+	Transform3D rot = new Transform3D();
 
-    // spin object has composited transformation matrix
-    Transform3D spin = new Transform3D();
-    Transform3D tempspin = new Transform3D();
+	trans.rotX(Math.PI/4.0d);
+	rot.rotY(Math.PI/5.0d);
+	trans.mul(rot);
+	trans.setScale(0.7);
+	trans.setTranslation(new Vector3d(-0.4, 0.3, 0.0));
 
-    spin.rotX(Math.PI/4.0d);
-    tempspin.rotY(Math.PI/5.0d);
-    spin.mul(tempspin);
-    spin.setScale(0.7);
-    spin.setTranslation(new Vector3d(-0.4, 0.3, 0.0));
+	TransformGroup objTrans = new TransformGroup(trans);
+	objRoot.addChild(objTrans);
 
-    TransformGroup objTrans = new TransformGroup(spin);
-    objRoot.addChild(objTrans);
+	// Create a simple shape leaf node, add it to the scene graph.
+	// ColorCube is a Convenience Utility class
+	objTrans.addChild(new ColorCube(0.4));
 
-    // Create a simple shape leaf node, add it to the scene graph.
-    // ColorCube is a Convenience Utility class
-    objTrans.addChild(new ColorCube(0.4));
-
-    //Create a raster obj
-    Shape3D shape = new Shape3D(drawRaster);
-    objRoot.addChild(shape);
-
-    // Let Java 3D perform optimizations on this scene graph.
-    objRoot.compile();
-
-    return objRoot;
-  }
-
-  public PrintFromButton ()
-  {
-  }
-
-    public void init() {
-	setLayout(new BorderLayout());
-	GraphicsConfiguration config =
-	    SimpleUniverse.getPreferredConfiguration();
-	
+	//Create a raster 
 	BufferedImage bImage = new BufferedImage(200, 200 ,
-						 BufferedImage.TYPE_INT_ARGB);
-
+						 BufferedImage.TYPE_INT_ARGB);        
 	ImageComponent2D buffer =
 	    new ImageComponent2D(ImageComponent.FORMAT_RGBA, bImage);
 	buffer.setCapability(ImageComponent2D.ALLOW_IMAGE_READ);
-
-	Raster drawRaster = new Raster(new Point3f(0.0f, 0.0f, 0.0f),
+	
+	drawRaster = new Raster(new Point3f(0.0f, 0.0f, 0.0f),
 				       Raster.RASTER_COLOR,
 				       0, 0, 200, 200, buffer, null);
 	
-	drawRaster.setCapability(Raster.ALLOW_IMAGE_WRITE);
-	
-	// create the main scene graph
-	BranchGroup scene = createSceneGraph(drawRaster);
-	
-	// create the on-screen canvas
-	Canvas3D d = new Canvas3D(config, false);
-	add("Center", d);
-	
-	// create a simple universe
-	u = new SimpleUniverse(d);
-	
+	drawRaster.setCapability(Raster.ALLOW_IMAGE_WRITE);        
+	Shape3D shape = new Shape3D(drawRaster);
+	objRoot.addChild(shape);
+
+	// Let Java 3D perform optimizations on this scene graph.
+	objRoot.compile();
+
+	return objRoot;
+    }
+
+    private OnScreenCanvas3D createOnScreenCanvasAndUniverse() {
+        // Get the preferred graphics configuration for the default screen
+	GraphicsConfiguration config =
+	    SimpleUniverse.getPreferredConfiguration();
+
+        // Create a Canvas3D using the preferred configuration
+	OnScreenCanvas3D onScrCanvas = new OnScreenCanvas3D(config, false);
+
+        // Create simple universe with view branch
+	univ = new SimpleUniverse(onScrCanvas);
+
 	// This will move the ViewPlatform back a bit so the
 	// objects in the scene can be viewed.
-	u.getViewingPlatform().setNominalViewingTransform();
-	
-	
-	// create an off Screen Buffer
-	
-	c = new OffScreenCanvas3D(config, true, drawRaster);
-	
+	univ.getViewingPlatform().setNominalViewingTransform();
+
+	// Ensure at least 5 msec per frame (i.e., < 200Hz)
+	univ.getViewer().getView().setMinimumFrameCycleTime(5);
+
+	return onScrCanvas;
+    }
+
+    private OffScreenCanvas3D createOffScreenCanvas() {
+	// request an offscreen Canvas3D with a single buffer configuration
+	GraphicsConfigTemplate3D template = new GraphicsConfigTemplate3D();
+	template.setDoubleBuffer(GraphicsConfigTemplate3D.UNNECESSARY);
+	GraphicsConfiguration gc = 
+                GraphicsEnvironment.getLocalGraphicsEnvironment().
+                getDefaultScreenDevice().getBestConfiguration(template);
+
+        // Create a offscreen Canvas3D using the single buffer configuration.
+        OffScreenCanvas3D offScrCanvas = 
+                new OffScreenCanvas3D(gc, true, drawRaster);
+
+        return offScrCanvas;
+    }
+
+    /**
+     * Creates new form PrintFromButton
+     */
+    public PrintFromButton() {
+        // Initialize the GUI components
+        JPopupMenu.setDefaultLightWeightPopupEnabled(false);
+        initComponents();
+
+        // Create the content branch and add it to the universe
+        scene = createSceneGraph();
+        
+        // Create an OnScreenCanvas3D and SimpleUniverse; add canvas to drawing panel
+        OnScreenCanvas3D onScreenCanvas = createOnScreenCanvasAndUniverse();
+        drawingPanel.add(onScreenCanvas, java.awt.BorderLayout.CENTER);	
+
+	// Creante an OffScreenCanvas3D
+        offScreenCanvas = createOffScreenCanvas();
+        
 	// set the offscreen to match the onscreen
-	Screen3D sOn = d.getScreen3D();
-	Screen3D sOff = c.getScreen3D();
+	Screen3D sOn = onScreenCanvas.getScreen3D();
+	Screen3D sOff = offScreenCanvas.getScreen3D();
 	sOff.setSize(sOn.getSize());
 	sOff.setPhysicalScreenWidth(sOn.getPhysicalScreenWidth());
 	sOff.setPhysicalScreenHeight(sOn.getPhysicalScreenHeight());
 	
 	// attach the same view to the offscreen canvas
-	u.getViewer().getView().addCanvas3D(c);
+	View view = univ.getViewer().getView();
+	view.addCanvas3D(offScreenCanvas);
 	
-	// create the gui
-	Button b = new Button ("Print");
-	b.addActionListener(this);
-	Panel p = new Panel();
-	p.add(b);
-	add("North", p);
-	
-	u.addBranchGraph(scene);
+        univ.addBranchGraph(scene);
     }
 
-    public void destroy() {
-	u.cleanup();
+    // ----------------------------------------------------------------
+    
+    /** This method is called from within the constructor to
+     * initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is
+     * always regenerated by the Form Editor.
+     */
+    // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        guiPanel = new javax.swing.JPanel();
+        myButton = new javax.swing.JButton();
+        drawingPanel = new javax.swing.JPanel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        fileMenu = new javax.swing.JMenu();
+        exitMenuItem = new javax.swing.JMenuItem();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Window Title");
+        guiPanel.setLayout(new java.awt.GridBagLayout());
+
+        myButton.setText("Print");
+        myButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                myButtonActionPerformed(evt);
+            }
+        });
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.insets = new java.awt.Insets(4, 4, 4, 4);
+        guiPanel.add(myButton, gridBagConstraints);
+
+        getContentPane().add(guiPanel, java.awt.BorderLayout.NORTH);
+
+        drawingPanel.setLayout(new java.awt.BorderLayout());
+
+        drawingPanel.setPreferredSize(new java.awt.Dimension(500, 500));
+        getContentPane().add(drawingPanel, java.awt.BorderLayout.CENTER);
+
+        fileMenu.setText("File");
+        exitMenuItem.setText("Exit");
+        exitMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                exitMenuItemActionPerformed(evt);
+            }
+        });
+
+        fileMenu.add(exitMenuItem);
+
+        jMenuBar1.add(fileMenu);
+
+        setJMenuBar(jMenuBar1);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void myButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_myButtonActionPerformed
+        offScreenCanvas.print(false);
+    }//GEN-LAST:event_myButtonActionPerformed
+
+    private void exitMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitMenuItemActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_exitMenuItemActionPerformed
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new PrintFromButton().setVisible(true);
+            }
+        });
     }
-  
-  public void actionPerformed(ActionEvent e)
-  {
-    // perform the print action
-    c.print(false);
-  }
-  
-  public static void main(String argv[])
-  {
-    new MainFrame(new PrintFromButton(), 500, 500);
-  }
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel drawingPanel;
+    private javax.swing.JMenuItem exitMenuItem;
+    private javax.swing.JMenu fileMenu;
+    private javax.swing.JPanel guiPanel;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JButton myButton;
+    // End of variables declaration//GEN-END:variables
+    
 }
