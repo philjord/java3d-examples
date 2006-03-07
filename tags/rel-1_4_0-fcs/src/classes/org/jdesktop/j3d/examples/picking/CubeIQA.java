ae2b82148a4fd090817f7ebd1b6e863a39334443
nobody <nobody@28c7f869-5b4e-e670-f602-82bfaf57f300>
2006-03-07 17:22:49 +0000

    This commit was manufactured by cvs2svn to create tag 'rel-1_4_0-fcs'.

---
+++ tags/rel-1_4_0-fcs/src/classes/org/jdesktop/j3d/examples/picking/CubeIQA.java
@@ -0,0 +1,138 @@
+/*
+ * $RCSfile$
+ *
+ * Copyright (c) 2006 Sun Microsystems, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistribution of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistribution in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * Neither the name of Sun Microsystems, Inc. or the names of
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * This software is provided "AS IS," without a warranty of any
+ * kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND
+ * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY
+ * EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL
+ * NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF
+ * USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR
+ * ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL,
+ * CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND
+ * REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR
+ * INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES.
+ *
+ * You acknowledge that this software is not designed, licensed or
+ * intended for use in the design, construction, operation or
+ * maintenance of any nuclear facility.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+
+package org.jdesktop.j3d.examples.picking;
+
+import javax.media.j3d.*;
+import javax.vecmath.*;
+
+class CubeIQA extends IndexedQuadArray {
+    CubeIQA() {
+	super(8, GeometryArray.COORDINATES | GeometryArray.COLOR_3, 24);
+
+
+	Point3f verts[] = new Point3f[8];
+	Color3f colors[] = new Color3f[8];
+	
+	verts[0] = new Point3f(1.0f, 1.0f,  1.0f);
+	verts[1] = new Point3f(-1.0f, 1.0f, 1.0f);
+	verts[2] = new Point3f(-1.0f,-1.0f, 1.0f);
+	verts[3] = new Point3f( 1.0f,-1.0f, 1.0f);
+	verts[4] = new Point3f( 1.0f, 1.0f,-1.0f);
+	verts[5] = new Point3f( -1.0f, 1.0f,-1.0f);
+	verts[6] = new Point3f( -1.0f,-1.0f,-1.0f);
+	verts[7] = new Point3f( 1.0f,-1.0f,-1.0f);
+	
+	colors[0] = new Color3f(1.0f, 0.0f, 0.0f);
+	colors[1] = new Color3f(0.0f, 1.0f, 0.0f);
+	colors[2] = new Color3f(0.0f, 0.0f, 1.0f);
+	colors[3] = new Color3f(1.0f, 1.0f, 0.0f);
+	colors[4] = new Color3f(1.0f, 0.0f, 1.0f);
+	colors[5] = new Color3f(0.0f, 1.0f, 1.0f);
+	colors[6] = new Color3f(0.0f, 1.0f, 1.0f);
+	colors[7] = new Color3f(0.0f, 1.0f, 1.0f);
+	
+	int pntsIndex[] = new int[24];
+	int clrsIndex[] = new int[24];
+	
+	pntsIndex[0] = 0;
+	clrsIndex[0] = 0;
+	pntsIndex[1] = 3;
+	clrsIndex[1] = 0;
+	pntsIndex[2] = 7;
+	clrsIndex[2] = 0;
+	pntsIndex[3] = 4;
+	clrsIndex[3] = 0;
+
+	pntsIndex[4] = 1;
+	clrsIndex[4] = 1;
+	pntsIndex[5] = 5;
+	clrsIndex[5] = 1;
+	pntsIndex[6] = 6;
+	clrsIndex[6] = 1;
+	pntsIndex[7] = 2;
+	clrsIndex[7] = 1;
+
+	pntsIndex[8]  = 0;
+	clrsIndex[8]  = 2;
+	pntsIndex[9]  = 4;
+	clrsIndex[9]  = 2;
+	pntsIndex[10] = 5;
+	clrsIndex[10] = 2;
+	pntsIndex[11] = 1;
+	clrsIndex[11] = 2;
+
+	pntsIndex[12] = 3;
+	clrsIndex[12] = 3;
+	pntsIndex[13] = 2;
+	clrsIndex[13] = 3;
+	pntsIndex[14] = 6;
+	clrsIndex[14] = 3;
+	pntsIndex[15] = 7;
+	clrsIndex[15] = 3;
+
+	pntsIndex[16] = 0;
+	clrsIndex[16] = 4;
+	pntsIndex[17] = 1;
+	clrsIndex[17] = 4;
+	pntsIndex[18] = 2;
+	clrsIndex[18] = 4;
+	pntsIndex[19] = 3;
+	clrsIndex[19] = 4;
+
+	pntsIndex[20] = 7;
+	clrsIndex[20] = 5;
+	pntsIndex[21] = 6;
+	clrsIndex[21] = 5;
+	pntsIndex[22] = 5;
+	clrsIndex[22] = 5;
+	pntsIndex[23] = 4;
+	clrsIndex[23] = 5;
+
+	setCoordinates(0, verts);
+	setCoordinateIndices(0, pntsIndex);
+	setColors(0, colors);
+	setColorIndices(0, clrsIndex);
+    }
+}
---
+++ tags/rel-1_4_0-fcs/src/classes/org/jdesktop/j3d/examples/virtual_input_device/VirtualInputDevice.java
@@ -0,0 +1,260 @@
+/*
+ * $RCSfile$
+ *
+ * Copyright (c) 2006 Sun Microsystems, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistribution of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistribution in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * Neither the name of Sun Microsystems, Inc. or the names of
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * This software is provided "AS IS," without a warranty of any
+ * kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND
+ * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY
+ * EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL
+ * NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF
+ * USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR
+ * ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL,
+ * CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND
+ * REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR
+ * INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES.
+ *
+ * You acknowledge that this software is not designed, licensed or
+ * intended for use in the design, construction, operation or
+ * maintenance of any nuclear facility.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+
+package org.jdesktop.j3d.examples.virtual_input_device;
+
+import javax.media.j3d.*;
+import javax.vecmath.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class VirtualInputDevice implements InputDevice {
+
+    private Vector3f position = new Vector3f();
+    private Transform3D newTransform = new Transform3D();
+    Sensor sensors[] = new Sensor[1];
+
+    // The wheel controls control the view platform orientation
+    private RotationControls rotControls;
+ 
+    // The button position controls control the view platform position
+    private PositionControls positionControls;
+
+    private Transform3D rotTransX = new Transform3D();
+    private Transform3D rotTransY = new Transform3D();
+    private Transform3D rotTransZ = new Transform3D();
+
+    private Vector3f initPos = new Vector3f();
+
+    private int processingMode;
+    private SensorRead sensorRead = new SensorRead();
+
+    // These are the settable parameters.
+    private boolean printvalues;
+    private int xscreeninitloc;
+    private int yscreeninitloc;
+    private int xscreensize;
+    private int yscreensize;
+    private float xobjinitloc;
+    private float yobjinitloc;
+    private float zobjinitloc;
+    private float xaxisrotinit;
+    private float yaxisrotinit;
+    private float zaxisrotinit;
+
+    /* 
+     * Create a device, and use the string arguments in args to construct
+     * the device with user preferences.
+     */
+    public VirtualInputDevice( String[] args ) {
+
+        // default user-definable values
+        printvalues = false;
+        xscreeninitloc = 400;
+        yscreeninitloc = 0;
+        xscreensize = 400;
+        yscreensize = 200;
+        xobjinitloc = 0.0f;
+        yobjinitloc = 0.0f;
+        zobjinitloc = 2.2f;
+        xaxisrotinit = 0.0f;
+        yaxisrotinit = 0.0f;
+        zaxisrotinit = 0.0f;
+
+
+        for(int i=0 ; i<args.length ; i+=2) {
+           if(args[i] == null)
+                  break;
+           else if(args[i] == "printvalues")
+                  printvalues =  (Boolean.valueOf(args[i+1])).booleanValue();
+           else if(args[i] == "xscreeninitloc")
+                  xscreeninitloc =  (Integer.valueOf(args[i+1])).intValue();
+           else if(args[i] == "yscreeninitloc")
+                  yscreeninitloc =  (Integer.valueOf(args[i+1])).intValue();
+           else if(args[i] == "xscreensize")
+                  xscreensize =  (Integer.valueOf(args[i+1])).intValue();
+           else if(args[i] == "yscreensize")
+                  yscreensize =  (Integer.valueOf(args[i+1])).intValue();
+           else if(args[i] == "xobjinitloc")
+                  xobjinitloc =  (Float.valueOf(args[i+1])).floatValue();
+           else if(args[i] == "yobjinitloc")
+                  yobjinitloc =  (Float.valueOf(args[i+1])).floatValue();
+           else if(args[i] == "zobjinitloc")
+                  zobjinitloc =  (Integer.valueOf(args[i+1])).floatValue();
+        }
+
+        if(printvalues ==  true) {
+           System.out.println("Initial values for VirtualInputDevice:");
+           System.out.println("xscreeninitloc = " + xscreeninitloc);
+           System.out.println("yscreeninitloc = " + yscreeninitloc);
+           System.out.println("xscreeninitsize = " + xscreensize);
+           System.out.println("yscreeninitsize = " + yscreensize);
+           System.out.println("xobjinitloc = " + xobjinitloc);
+           System.out.println("yobjinitloc = " + yobjinitloc);
+           System.out.println("zobjinitloc = " + zobjinitloc);
+           System.out.println("xaxisrotinit = " + xaxisrotinit);
+           System.out.println("yaxisrotinit = " + yaxisrotinit);
+           System.out.println("zaxisrotinit = " + zaxisrotinit);
+        }
+
+
+        // initialize the InputDevice GUI
+        Frame deviceFrame = new Frame();
+        deviceFrame.setSize(xscreensize,yscreensize);
+        deviceFrame.setLocation(xscreeninitloc, yscreeninitloc);
+        deviceFrame.setTitle("Virtual Input Device");
+        ButtonPositionControls positionControls;
+        // initialize position with initial x, y, and z position
+        positionControls = new ButtonPositionControls( xobjinitloc, 
+                                                  yobjinitloc, zobjinitloc);
+        WheelControls rotControls;
+        // initialize rotations with initial angles in radians)
+        rotControls = new WheelControls(xaxisrotinit, yaxisrotinit,
+                                                            zaxisrotinit);
+        positionControls.setDevice (this);
+        Panel devicePanel = new Panel();
+        devicePanel.setLayout( new BorderLayout() );
+        devicePanel.add("East", positionControls );
+        devicePanel.add("West", rotControls );
+        deviceFrame.add( devicePanel );
+        deviceFrame.pack();
+        deviceFrame.setVisible(true);
+
+        initPos.set(xobjinitloc, yobjinitloc, zobjinitloc);
+
+        this.positionControls = positionControls;
+        this.rotControls = rotControls;
+        
+        // default processing mode
+        processingMode = InputDevice.DEMAND_DRIVEN;
+
+	sensors[0] = new Sensor(this);
+    }
+
+    public void close() {
+    }
+
+    public int getProcessingMode() {
+        return processingMode;
+    }
+
+    public int getSensorCount() {
+	return sensors.length;
+    }
+
+    public Sensor getSensor( int sensorIndex ) {
+	return sensors[sensorIndex];
+    }
+
+    public boolean initialize() {
+	return true;
+    }
+
+    public void pollAndProcessInput() {
+
+        sensorRead.setTime( System.currentTimeMillis() );
+ 
+        rotTransX.rotX(-rotControls.getXAngle());
+        rotTransY.rotY(-rotControls.getYAngle());
+        rotTransZ.rotZ(-rotControls.getZAngle());
+ 
+        positionControls.getPosition(position);
+        newTransform.set(position);
+        newTransform.mul( rotTransX );
+
+        newTransform.mul(rotTransY);
+        newTransform.mul(rotTransZ);
+
+        sensorRead.set( newTransform );
+        sensors[0].setNextSensorRead( sensorRead );
+    }
+
+
+    public void processStreamInput() {
+    }
+
+
+    public void setNominalPositionAndOrientation() {
+
+        sensorRead.setTime( System.currentTimeMillis() );
+ 
+        rotTransX.rotX(xaxisrotinit);
+        rotTransY.rotY(yaxisrotinit);
+        rotTransZ.rotZ(zaxisrotinit);
+ 
+        position.set(initPos);
+        
+        newTransform.set( position );
+ 
+        newTransform.mul(rotTransX);  
+        newTransform.mul(rotTransY);
+        newTransform.mul(rotTransZ);
+ 
+        sensorRead.set( newTransform );
+        sensors[0].setNextSensorRead( sensorRead );
+        rotControls.reset();
+        positionControls.setPosition(initPos);
+    }
+
+
+
+    public void setProcessingMode( int mode ) {
+
+         // A typical driver might implement only one of these modes, and
+         // throw an exception when there is an attempt to switch modes.
+         // However, this example allows one to use any processing mode.
+
+         switch(mode) {
+            case InputDevice.DEMAND_DRIVEN:
+            case InputDevice.NON_BLOCKING:
+            case InputDevice.BLOCKING:
+                 processingMode = mode;
+            break;
+            default:
+               throw new IllegalArgumentException("Processing mode must " +
+                       "be one of DEMAND_DRIVEN, NON_BLOCKING, or BLOCKING");
+         }
+    }
+
+}
