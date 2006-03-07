f12a0856f4cfaa0316e131c707faee40d2079123
nobody <nobody@28c7f869-5b4e-e670-f602-82bfaf57f300>
2006-03-07 17:42:45 +0000

    This commit was manufactured by cvs2svn to create tag 'chk-1_5_0-pre-jogl'.

---
+++ tags/chk-1_5_0-pre-jogl/src/classes/org/jdesktop/j3d/examples/lod/LOD.java
@@ -0,0 +1,224 @@
+/*
+ * $RCSfile$
+ *
+ * Copyright (c) 2006 Sun Microsystems, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistribution of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistribution in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * Neither the name of Sun Microsystems, Inc. or the names of
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * This software is provided "AS IS," without a warranty of any
+ * kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND
+ * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY
+ * EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL
+ * NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF
+ * USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR
+ * ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL,
+ * CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND
+ * REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR
+ * INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES.
+ *
+ * You acknowledge that this software is not designed, licensed or
+ * intended for use in the design, construction, operation or
+ * maintenance of any nuclear facility.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+
+package org.jdesktop.j3d.examples.lod;
+
+import com.sun.j3d.utils.geometry.*;
+import com.sun.j3d.utils.behaviors.vp.*;
+import com.sun.j3d.utils.universe.*;
+import javax.media.j3d.*;
+import javax.vecmath.*;
+import java.awt.GraphicsConfiguration;
+
+/**
+ * Simple Java 3D example program to display a spinning cube.
+ */
+public class LOD extends javax.swing.JFrame {
+
+    private SimpleUniverse univ = null;
+    private BranchGroup scene = null;
+
+    public BranchGroup createSceneGraph() {
+	// Create the root of the branch graph
+	BranchGroup objRoot = new BranchGroup();
+
+	createLights(objRoot);
+
+	// Create the transform group node and initialize it to the
+	// identity.  Enable the TRANSFORM_WRITE capability so that
+	// our behavior code can modify it at runtime.  Add it to the
+	// root of the subgraph.
+	TransformGroup objTrans = new TransformGroup();
+	objTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+	objTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
+	objRoot.addChild(objTrans);
+
+	// Create a switch to hold the different levels of detail
+	Switch sw = new Switch(0);
+	sw.setCapability(javax.media.j3d.Switch.ALLOW_SWITCH_READ);
+	sw.setCapability(javax.media.j3d.Switch.ALLOW_SWITCH_WRITE);
+
+
+	// Create several levels for the switch, with less detailed
+	// spheres for the ones which will be used when the sphere is
+	// further away
+	sw.addChild(new Sphere(0.4f, Sphere.GENERATE_NORMALS, 40));
+	sw.addChild(new Sphere(0.4f, Sphere.GENERATE_NORMALS, 20));
+	sw.addChild(new Sphere(0.4f, Sphere.GENERATE_NORMALS, 10));
+	sw.addChild(new Sphere(0.4f, Sphere.GENERATE_NORMALS, 3));
+
+	// Add the switch to the main group
+	objTrans.addChild(sw);
+
+	BoundingSphere bounds =
+	    new BoundingSphere(new Point3d(0.0,0.0,0.0), 100.0);
+
+	// set up the DistanceLOD behavior
+	float[] distances = new float[3];
+	distances[0] = 5.0f;
+	distances[1] = 10.0f;
+	distances[2] = 25.0f;
+	DistanceLOD lod = new DistanceLOD(distances);
+	lod.addSwitch(sw);
+	lod.setSchedulingBounds(bounds);
+	objTrans.addChild(lod);
+
+        // Have Java 3D perform optimizations on this scene graph.
+        objRoot.compile();
+
+	return objRoot;
+    }
+
+    private void createLights(BranchGroup graphRoot) {
+
+        // Create a bounds for the light source influence
+        BoundingSphere bounds =
+            new BoundingSphere(new Point3d(0.0,0.0,0.0), 100.0);
+
+        // Set up the global, ambient light
+        Color3f alColor = new Color3f(0.2f, 0.2f, 0.2f);
+        AmbientLight aLgt = new AmbientLight(alColor);
+        aLgt.setInfluencingBounds(bounds);
+        graphRoot.addChild(aLgt);
+
+        // Set up the directional (infinite) light source
+        Color3f lColor1 = new Color3f(0.9f, 0.9f, 0.9f);
+        Vector3f lDir1  = new Vector3f(1.0f, 1.0f, -1.0f);
+        DirectionalLight lgt1 = new DirectionalLight(lColor1, lDir1);
+        lgt1.setInfluencingBounds(bounds);
+        graphRoot.addChild(lgt1);
+    }
+
+    private Canvas3D createUniverse() {
+	// Get the preferred graphics configuration for the default screen
+	GraphicsConfiguration config =
+	    SimpleUniverse.getPreferredConfiguration();
+
+	// Create a Canvas3D using the preferred configuration
+	Canvas3D c = new Canvas3D(config);
+
+	// Create simple universe with view branch
+	univ = new SimpleUniverse(c);
+
+        // only add zoom mouse behavior to viewingPlatform
+	ViewingPlatform viewingPlatform = univ.getViewingPlatform();
+	
+        // This will move the ViewPlatform back a bit so the
+        // objects in the scene can be viewed.
+	viewingPlatform.setNominalViewingTransform();
+
+	// add orbit behavior to the ViewingPlatform, but disable rotate
+	// and translate
+	OrbitBehavior orbit = new OrbitBehavior(c,
+						OrbitBehavior.REVERSE_ZOOM |
+						OrbitBehavior.DISABLE_ROTATE |
+						OrbitBehavior.DISABLE_TRANSLATE);
+	BoundingSphere bounds =
+	    new BoundingSphere(new Point3d(0.0, 0.0, 0.0), 100.0);
+	orbit.setSchedulingBounds(bounds);
+	viewingPlatform.setViewPlatformBehavior(orbit);       
+        
+	// This will move the ViewPlatform back a bit so the
+	// objects in the scene can be viewed.
+	univ.getViewingPlatform().setNominalViewingTransform();
+
+	// Ensure at least 5 msec per frame (i.e., < 200Hz)
+	univ.getViewer().getView().setMinimumFrameCycleTime(5);
+
+	return c;
+    }
+
+    /**
+     * Creates new form LOD
+     */
+    public LOD() {
+	// Initialize the GUI components
+	initComponents();
+
+	// Create Canvas3D and SimpleUniverse; add canvas to drawing panel
+	Canvas3D c = createUniverse();
+	drawingPanel.add(c, java.awt.BorderLayout.CENTER);
+
+	// Create the content branch and add it to the universe
+	scene = createSceneGraph();
+	univ.addBranchGraph(scene);
+    }
+
+    // ----------------------------------------------------------------
+    
+    /** This method is called from within the constructor to
+     * initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is
+     * always regenerated by the Form Editor.
+     */
+    // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        drawingPanel = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("LOD");
+        drawingPanel.setLayout(new java.awt.BorderLayout());
+
+        drawingPanel.setPreferredSize(new java.awt.Dimension(700, 700));
+        getContentPane().add(drawingPanel, java.awt.BorderLayout.CENTER);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LOD().setVisible(true);
+            }
+        });
+    }
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel drawingPanel;
+    // End of variables declaration//GEN-END:variables
+    
+}
