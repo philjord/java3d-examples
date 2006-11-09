59ef25109b7243ebff8a130810bea0058e08c4ac
pepe <pepe@28c7f869-5b4e-e670-f602-82bfaf57f300>
2006-11-09 06:47:37 +0000

    Issue 131: updated demo for JCanvas3D. Removed tabs, table and added options.

--- src/classes/org/jdesktop/j3d/examples/jcanvas3d/JInternalWorld.java
+++ src/classes/org/jdesktop/j3d/examples/jcanvas3d/JInternalWorld.java
@@ -41,97 +41,93 @@
  * $Date$
  * $State$
  */
-
 package org.jdesktop.j3d.examples.jcanvas3d;
 
 import com.sun.j3d.exp.swing.JCanvas3D;
-//import com.sun.j3d.exp.swing.Canvas3DWrapper;
 import com.sun.j3d.utils.behaviors.mouse.MouseRotate;
 import com.sun.j3d.utils.geometry.ColorCube;
 import com.sun.j3d.utils.universe.SimpleUniverse;
+
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+
 import javax.media.j3d.Alpha;
 import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
+import javax.media.j3d.GraphicsConfigTemplate3D;
 import javax.media.j3d.RotationInterpolator;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
+
 import javax.swing.JInternalFrame;
-import javax.swing.table.DefaultTableModel;
+
 import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
 
 
 /**
- * DOCUMENT ME!
+ * This is a JInternalFrame holding an universe, which can be configured to
+ * be interactive -that is, where user can interact with object- or automatic
+ * -where the object spins only-. When in automatic mode, spinning speed is
+ * changed so that they look less the same. Changing the spinning start angle
+ * helps unsynchronizing the rotations too.
  *
  * @author pepe
  */
 public class JInternalWorld extends JInternalFrame {
     /** DOCUMENT ME! */
-    JCanvas3D c;
-
-    /** Creates a new instance of JInternalWorld */
-    private SimpleUniverse u = null;
-
-    /** DOCUMENT ME! */
     private Component comp;
-
-    private DefaultTableModel model;
 
     /**
      * Creates a new JInternalWorld object.
      *
-     * @param isDelayed DOCUMENT ME!
-     * @param isInteractive DOCUMENT ME!
+     * @param isInteractive tells the world to be constructed as interactive
+     * @param isDelayed tells the rotator to start at a random alpha.
      */
-    public JInternalWorld(boolean isDelayed, boolean isInteractive, DefaultTableModel model) {
+    public JInternalWorld(boolean isInteractive, boolean isDelayed) {
+        super();
         setSize(256, 256);
         setClosable(true);
-        this.model = model;
-        model.addRow( new Object[]{new Integer( hashCode()), Integer.valueOf(0) });
 
-        c = new JCanvas3D();
+        JCanvas3D canvas = new JCanvas3D(new GraphicsConfigTemplate3D());
 
         if (true == isDelayed) {
-            c.setResizeMode(c.RESIZE_DELAYED);
+            canvas.setResizeMode(canvas.RESIZE_DELAYED);
         }
 
-        setComp(c);
+        comp = canvas;
+
         Dimension dim = new Dimension(256, 256);
-        getComp().setPreferredSize( dim );
-        getComp().setSize( dim );
+        comp.setPreferredSize(dim);
+        comp.setSize(dim);
         getContentPane().setLayout(new BorderLayout());
-        getContentPane().add(getComp(), BorderLayout.CENTER);
+        getContentPane().add(comp, BorderLayout.CENTER);
         pack();
 
         // Create a simple scene and attach it to the virtual universe
-        BranchGroup scene = createSceneGraph(isInteractive);
-        u = new SimpleUniverse(c.getOffscreenCanvas3D());//TODO: this is awful and must not be done like that in final version
+        BranchGroup scene = createSceneGraph(isInteractive, isDelayed);
+        SimpleUniverse universe = new SimpleUniverse(canvas.getOffscreenCanvas3D()); //TODO: this is awful and must not be done like that in final version
 
         // This will move the ViewPlatform back a bit so the
         // objects in the scene can be viewed.
-        u.getViewingPlatform().setNominalViewingTransform();
-//        Canvas3DWrapper wrapper = new Canvas3DWrapper( c );
-//        wrapper.setView( u.getViewer().getView() );
-//        KeyNavigatorBehavior navig = new KeyNavigatorBehavior(u.getViewingPlatform().getViewPlatformTransform() );
-//        BranchGroup navigBG = new BranchGroup();
-//        navigBG.addChild( navig );
-//        scene.addChild( navigBG );
-        u.addBranchGraph(scene);
-//        c.getOffscreenCanvas3D().getView().setMinimumFrameCycleTime(200);//TODO: this is awful and must not be done like that in final version
+        universe.getViewingPlatform().setNominalViewingTransform();
+        universe.getViewer().getView().setMinimumFrameCycleTime(30);
+        universe.addBranchGraph(scene);
     }
 
     /**
-     * DOCUMENT ME!
+     * Creates the world. Only exists to cleanup the source a bit
      *
-     * @param isInteractive DOCUMENT ME!
+     * @param isInteractive tells the world to be constructed as interactive
+     * @param isDelayed tells the rotator to start at a random alpha.
      *
-     * @return DOCUMENT ME!
+     * @return a global branchgroup containing the world, as desired.
      */
-    public BranchGroup createSceneGraph(boolean isInteractive) {
+    private BranchGroup createSceneGraph(boolean isInteractive,
+        boolean isDelayed) {
         // Create the root of the branch graph
         BranchGroup objRoot = new BranchGroup();
 
@@ -141,12 +137,13 @@ public class JInternalWorld extends JInternalFrame {
         // the root of the subgraph.
         TransformGroup objTrans = new TransformGroup();
         Transform3D t3dTrans = new Transform3D();
-        t3dTrans.setTranslation( new Vector3d( 0, 0, -1 ) );
-        objTrans.setTransform( t3dTrans );
+        t3dTrans.setTranslation(new Vector3d(0, 0, -1));
+        objTrans.setTransform(t3dTrans);
+
         TransformGroup objRot = new TransformGroup();
         objRot.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
         objRoot.addChild(objTrans);
-        objTrans.addChild( objRot );
+        objTrans.addChild(objRot);
 
         // Create a simple Shape3D node; add it to the scene graph.
         ColorCube cube = new ColorCube(0.75);
@@ -156,7 +153,7 @@ public class JInternalWorld extends JInternalFrame {
                 100.0);
 
         if (true == isInteractive) {
-            MouseRotate mr = new MouseRotate(getComp(), objRot);
+            MouseRotate mr = new MouseRotate(comp, objRot);
             mr.setSchedulingBounds(bounds);
             mr.setSchedulingInterval(1);
             objRoot.addChild(mr);
@@ -165,11 +162,23 @@ public class JInternalWorld extends JInternalFrame {
             // desired operation on the specified transform and add
             // it into the scene graph.
             Transform3D yAxis = new Transform3D();
-            Alpha rotationAlpha = new Alpha(-1,
-                    Math.max(2000, (int) (Math.random() * 8000.)));
+
+            // rotation speed is randomized a bit so that it does not go at the same speed on every canvases,
+            // which will make it more natural and express the differences between every present universes
+            int duration = Math.max(2000, (int) (Math.random() * 8000.));
+            Alpha rotationAlpha = null;
+
+            if (true == isDelayed) {
+                rotationAlpha = new Alpha(-1,
+                        (int) ((double) duration * Math.random()), 0, duration,
+                        0, 0);
+            } else {
+                rotationAlpha = new Alpha(-1, duration);
+            }
 
             RotationInterpolator rotator = new RotationInterpolator(rotationAlpha,
                     objRot, yAxis, 0.0f, (float) Math.PI * 2.0f);
+
             rotator.setSchedulingBounds(bounds);
             objRoot.addChild(rotator);
         }
@@ -177,21 +186,4 @@ public class JInternalWorld extends JInternalFrame {
         return objRoot;
     }
 
-    /**
-     * DOCUMENT ME!
-     *
-     * @return DOCUMENT ME!
-     */
-    protected Component getComp() {
-        return comp;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param comp DOCUMENT ME!
-     */
-    protected void setComp(Component comp) {
-        this.comp = comp;
-    }
 }
