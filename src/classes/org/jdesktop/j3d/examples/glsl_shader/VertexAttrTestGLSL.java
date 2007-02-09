10b0f946849a38390bde913865363d169d05e1bb
kcr <kcr@28c7f869-5b4e-e670-f602-82bfaf57f300>
2007-02-09 17:21:55 +0000

    Update copyright to 2007

--- src/classes/org/jdesktop/j3d/examples/glsl_shader/VertexAttrTestGLSL.java
+++ src/classes/org/jdesktop/j3d/examples/glsl_shader/VertexAttrTestGLSL.java
@@ -1,7 +1,7 @@
 /*
  * $RCSfile$
  *
- * Copyright (c) 2006 Sun Microsystems, Inc. All rights reserved.
+ * Copyright (c) 2007 Sun Microsystems, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -41,6 +41,7 @@
  * $Date$
  * $State$
  */
+
 package org.jdesktop.j3d.examples.glsl_shader;
 
 import com.sun.j3d.utils.universe.*;
