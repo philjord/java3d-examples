a32388def2ee2ca51f29f0e70c474eed2e321470
nobody <nobody@28c7f869-5b4e-e670-f602-82bfaf57f300>
2007-04-06 23:33:34 +0000

    This commit was manufactured by cvs2svn to create tag 'exp-1_5_1-build2'.

---
+++ tags/exp-1_5_1-build2/src/classes/org/jdesktop/j3d/examples/sound/BackgroundSoundBehavior.java
@@ -0,0 +1,79 @@
+/*
+ * $RCSfile$
+ *
+ * Copyright (c) 2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistribution of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistribution in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * Neither the name of Sun Microsystems, Inc. or the names of
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * This software is provided "AS IS," without a warranty of any
+ * kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND
+ * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY
+ * EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL
+ * NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF
+ * USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR
+ * ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL,
+ * CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND
+ * REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR
+ * INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES.
+ *
+ * You acknowledge that this software is not designed, licensed or
+ * intended for use in the design, construction, operation or
+ * maintenance of any nuclear facility.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+
+package org.jdesktop.j3d.examples.sound;
+
+import java.net.URL;
+import java.util.Enumeration;
+import javax.media.j3d.*;
+import javax.vecmath.*;
+import org.jdesktop.j3d.examples.Resources;
+
+public class BackgroundSoundBehavior extends Behavior {
+    
+    private WakeupCondition condition = new WakeupOnElapsedFrames(0);
+ 
+    /** Creates a new instance of BackgroundSoundBehavior */
+    public BackgroundSoundBehavior(BackgroundSound bgs, URL url) {
+                
+        Bounds b = new BoundingSphere(new Point3d(), 20);
+        bgs.setSoundData(new MediaContainer(url));
+        bgs.setEnable(true);
+        bgs.setPause(false);
+        bgs.setLoop(-1);        
+        bgs.setContinuousEnable(true);
+        bgs.setSchedulingBounds(b);
+        
+    }
+
+    public void initialize() {
+        
+        wakeupOn(condition);
+    }
+    
+    public void processStimulus(Enumeration enumeration) {        
+        wakeupOn(condition);
+    }
+    
+}
